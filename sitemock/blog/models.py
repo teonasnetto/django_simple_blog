0d1d771be5202c219393d415e54b5f2a534fce4a
Teonas <teonasnetto@gmail.com>
2023-02-15 11:45:41 -0300

    fix post list and get_absolute_url

--- sitemock/blog/models.py
+++ sitemock/blog/models.py
@@ -34,7 +34,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        return reverse("article_detail", kwargs={"slug": self.slug})
+        return reverse("blog:post_detail", kwargs={"slug": self.slug})
 
     def save(self, *args, **kwargs):
         if not self.slug:
